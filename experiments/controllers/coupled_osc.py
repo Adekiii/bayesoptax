1ef3bb4940a1e1fc2aee8bde98ef7f3c438b684f
adekiii <ademk98@hotmail.com>
2026-04-09 11:47:42 +0200

    add coupled oscilator controller

---
+++ experiments/controllers/coupled_osc.py
@@ -0,0 +1,36 @@
+# Adapted from:
+# Generative Modeling of Neural Dynamics via Latent Stochastic Differential Equations, ElGazzar. 2024
+# https://arxiv.org/abs/2412.12112 
+import jax.numpy as jnp
+import jax.random as jr
+
+
+def init_params(key, n_osc, n_in=2, n_out=2):
+    k1, k2 = jr.split(key)
+    return {
+        "alpha": jnp.zeros(n_osc),
+        "omega": jr.normal(k1, (n_osc,)),
+        "W_kappa": jr.normal(k2, (n_in,)) * 0.1,
+        "b_kappa": jnp.zeros(()),
+        "V": jnp.zeros((n_out, 2 * n_osc))
+    }
+
+
+def step(t, osc_state, plant_state, params):
+    n = len(osc_state) // 2
+    a, b = osc_state[:n], osc_state[n:]
+
+    alpha = params["alpha"]
+    omega = params["omega"]
+    kappa = jnp.tanh(params["W_kappa"] @ plant_state + params["b_kappa"])
+
+    r_sq = a**2 + b**2
+    a_mean = jnp.mean(a)
+    b_mean = jnp.mean(b)
+
+    da = alpha * a - omega * b - r_sq * a + kappa * a_mean
+    db = omega * a + alpha * b - r_sq * b + kappa * b_mean
+
+    state_dot = jnp.concatenate([da, db])
+    u = params["V"] @ jnp.tanh(osc_state)
+    return state_dot, u
